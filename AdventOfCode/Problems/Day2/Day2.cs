91b43bb49bf5df1eca455f43508d86f2deb511f4
Andrew <blazersand2000@yahoo.com>
2022-12-07 21:18:37 -0500

    Finished day 2, part 1

--- AdventOfCode/Problems/Day2/Day2.cs
+++ AdventOfCode/Problems/Day2/Day2.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace AdventOfCode.Problems.Day2
@@ -8,7 +9,56 @@ namespace AdventOfCode.Problems.Day2
    {
       public void Run()
       {
-         throw new NotImplementedException();
+         var lines = File.ReadAllLines("Problems/Day2/input.txt");
+
+         var myScore = 0;
+         var opponentScore = 0;
+
+         var scoreLookup = new Dictionary<string, int>
+         {
+            { "A X", 4 },
+            { "A Y", 8 },
+            { "A Z", 3 },
+            { "B X", 1 },
+            { "B Y", 5 },
+            { "B Z", 9 },
+            { "C X", 7 },
+            { "C Y", 2 },
+            { "C Z", 6 },
+         };
+
+         foreach(var line in lines)
+         {
+            myScore += scoreLookup[line];
+         }
+
+         Console.WriteLine(myScore);
+      }
+
+      private Shape ConvertToShape(char letter)
+      {
+         switch (letter)
+         {
+            case 'A':
+            case 'X':
+               return Shape.Rock;
+            case 'B':
+            case 'Y':
+               return Shape.Paper;
+            case 'C':
+            case 'Z':
+               return Shape.Scissors;
+            default:
+               throw new ApplicationException("Unexpected input");
+         }
       }
    }
+
+   public enum Shape
+   {
+      Rock = 1,
+      Paper = 2,
+      Scissors = 3
+   }
 }
+
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -11,11 +11,11 @@ namespace AdventOfCode
       static void Main(string[] args)
       {
          var services = new ServiceCollection();
-         Console.WriteLine("Hello World!");
 
          var problems = Assembly.GetExecutingAssembly().DefinedTypes.Where(ti => ti.ImplementedInterfaces.Contains(typeof(IProblem)));
          var mostRecentProblem = problems.OrderBy(p => p.Name.Substring(3)).Last();
          services.AddScoped(typeof(IProblem), mostRecentProblem);
+         Console.WriteLine($"Running {mostRecentProblem.Name}\n", ConsoleColor.Blue);
          var serviceProvider = services.BuildServiceProvider();
 
          var problem = serviceProvider.GetService<IProblem>();
