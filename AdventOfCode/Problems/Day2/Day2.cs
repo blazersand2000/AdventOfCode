f014168a9b6702954c844eea12dd19a1c5924cd8
Andrew <blazersand2000@yahoo.com>
2022-12-08 18:27:47 -0500

    Finished day 2

--- AdventOfCode/Problems/Day2/Day2.cs
+++ AdventOfCode/Problems/Day2/Day2.cs
@@ -11,28 +11,98 @@ namespace AdventOfCode.Problems.Day2
       {
          var lines = File.ReadAllLines("Problems/Day2/input.txt");
 
+         Part1(lines);
+         Part2(lines);
+      }
+
+      private void Part1(string[] lines)
+      {
          var myScore = 0;
-         var opponentScore = 0;
 
-         var scoreLookup = new Dictionary<string, int>
+         foreach (var line in lines)
          {
-            { "A X", 4 },
-            { "A Y", 8 },
-            { "A Z", 3 },
-            { "B X", 1 },
-            { "B Y", 5 },
-            { "B Z", 9 },
-            { "C X", 7 },
-            { "C Y", 2 },
-            { "C Z", 6 },
-         };
+            var lineChars = GetLineChars(line);
+            var myShape = ConvertToShape(lineChars[1]);
+            var opponentShape = ConvertToShape(lineChars[0]);
 
-         foreach(var line in lines)
-         {
-            myScore += scoreLookup[line];
+            myScore += GetRoundScore(myShape, opponentShape) + (int)myShape;
          }
 
+         Console.WriteLine("Part 1:");
          Console.WriteLine(myScore);
+      }
+
+      private void Part2(string[] lines)
+      {
+         var myScore = 0;
+
+         foreach (var line in lines)
+         {
+            var lineChars = GetLineChars(line);
+            var opponentShape = ConvertToShape(lineChars[0]);
+            var desiredOutcome = ConvertToOutcome(lineChars[1]);
+            var myShape = GetShapeForOutcome(opponentShape, desiredOutcome);
+
+            myScore += GetRoundScore(myShape, opponentShape) + (int)myShape;
+         }
+
+         Console.WriteLine("Part 2:");
+         Console.WriteLine(myScore);
+      }
+
+      private char[] GetLineChars(string line)
+      {
+         var splitLine = line.Split(' ');
+         return new char[] { splitLine[0][0], splitLine[1][0] };
+      }
+
+      private int GetRoundScore(Shape myShape, Shape opponentShape)
+      {
+         if (myShape == opponentShape)
+         {
+            return (int)Outcome.Draw;
+         }
+         if (myShape - opponentShape == 1 || myShape - opponentShape == -2)
+         {
+            return (int)Outcome.Win;
+         }
+         return (int)Outcome.Lose;
+      }
+
+      private Shape GetShapeForOutcome(Shape opponentShape, Outcome desiredOutcome)
+      {
+         switch (opponentShape)
+         {
+            case Shape.Rock:
+               switch (desiredOutcome)
+               {
+                  case Outcome.Lose:
+                     return Shape.Scissors;
+                  case Outcome.Win:
+                     return Shape.Paper;
+               }
+               break;
+            case Shape.Paper:
+               switch (desiredOutcome)
+               {
+                  case Outcome.Lose:
+                     return Shape.Rock;
+                  case Outcome.Win:
+                     return Shape.Scissors;
+               }
+               break;
+            case Shape.Scissors:
+               switch (desiredOutcome)
+               {
+                  case Outcome.Lose:
+                     return Shape.Paper;
+                  case Outcome.Win:
+                     return Shape.Rock;
+               }
+               break;
+         }
+
+         return opponentShape;
       }
 
       private Shape ConvertToShape(char letter)
@@ -52,6 +122,21 @@ namespace AdventOfCode.Problems.Day2
                throw new ApplicationException("Unexpected input");
          }
       }
+
+      private Outcome ConvertToOutcome(char letter)
+      {
+         switch (letter)
+         {
+            case 'X':
+               return Outcome.Lose;
+            case 'Y':
+               return Outcome.Draw;
+            case 'Z':
+               return Outcome.Win;
+            default:
+               throw new ApplicationException("Unexpected input");
+         }
+      }
    }
 
    public enum Shape
@@ -59,6 +144,13 @@ namespace AdventOfCode.Problems.Day2
       Rock = 1,
       Paper = 2,
       Scissors = 3
+   }
+
+   public enum Outcome
+   {
+      Lose = 0,
+      Draw = 3,
+      Win = 6
    }
 }
 
