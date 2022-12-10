001b3a11795f047f4ba4032dd245690175b8bc4d
Andrew <blazersand2000@yahoo.com>
2022-12-10 13:27:52 -0500

    Finished day 4

---
+++ AdventOfCode/Problems/Day4/Day4.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Problems.Day2
+{
+   public class Day4 : IProblem
+   {
+      public void Run()
+      {
+         var lines = File.ReadAllLines("Problems/Day4/input.txt");
+
+         Part1(lines);
+         Part2(lines);
+      }
+
+      private void Part1(string[] lines)
+      {
+         var numFullyContained = 0;
+
+         foreach (var line in lines)
+         {
+            var ranges = GetRanges(line);
+            if (AnyFullyContained(ranges))
+            {
+               numFullyContained++;
+            }
+         }
+
+         Console.WriteLine("Part 1:");
+         Console.WriteLine(numFullyContained);
+      }
+
+      private void Part2(string[] lines)
+      {
+         var numOverlap = 0;
+
+         foreach (var line in lines)
+         {
+            var ranges = GetRanges(line);
+            if (AnyOverlap(ranges) || AnyFullyContained(ranges))
+            {
+               numOverlap++;
+            }
+         }
+
+         Console.WriteLine("Part 2:");
+         Console.WriteLine(numOverlap);
+      }
+
+      private int[][] GetRanges(string line)
+      {
+         return line.Split(',').Select(r => r.Split('-').ToArray().Select(n => int.Parse(n)).ToArray()).ToArray();
+      }
+
+      private bool AnyFullyContained(int[][] ranges)
+      {
+         var rangeA = ranges[0];
+         var rangeB = ranges[1];
+
+         if (rangeA[0] <= rangeB[0] && rangeA[1] >= rangeB[1])
+         {
+            return true;
+         }
+         if (rangeB[0] <= rangeA[0] && rangeB[1] >= rangeA[1])
+         {
+            return true;
+         }
+
+         return false;
+      }
+
+      private bool AnyOverlap(int[][] ranges)
+      {
+         var rangeA = ranges[0];
+         var rangeB = ranges[1];
+
+         if (rangeA[0] <= rangeB[1] && rangeB[1] <= rangeA[1])
+         {
+            return true;
+         }
+         if (rangeA[0] <= rangeB[0] && rangeB[0] <= rangeA[1])
+         {
+            return true;
+         }
+
+         return false;
+      }
+   }
+}
+
