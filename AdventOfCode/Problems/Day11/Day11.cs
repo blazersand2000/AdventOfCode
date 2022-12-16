4450d9044c2abca4fabae1496809d31b61b8bd77
Andrew <blazersand2000@yahoo.com>
2022-12-15 23:22:23 -0500

    Day 11 in progress

---
+++ AdventOfCode/Problems/Day11/Day11.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Problems.Day11
+{
+   public class Day11 : IProblem
+   {
+      public void Run()
+      {
+         var lines = File.ReadAllLines("Problems/Day11/input.txt");
+
+         Console.WriteLine("Part 1:");
+
+         Console.WriteLine("Part 2:");
+      }
+   }
+}
+
---
+++ AdventOfCode/Problems/Day11/Monkey.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Problems.Day11
+{
+   public class Monkey
+   {
+      private readonly Queue<int> _items;
+      private readonly string _operation;
+      private readonly int _divisorTest;
+      private readonly Dictionary<bool, int> _testResultMonkey;
+
+      public Monkey(List<int> items, string operation, int divisorTest, int toMonkeyOnFalse, int toMonkeyOnTrue)
+      {
+         _items = new Queue<int>(items);
+         _operation = operation;
+         _divisorTest = divisorTest;
+         _testResultMonkey = new Dictionary<bool, int>
+         {
+            { false, toMonkeyOnFalse },
+            { true, toMonkeyOnTrue },
+         };
+      }
+
+      public async Task<Dictionary<int, Queue<int>>> InspectAllItems()
+      {
+         var tossedItems = new Dictionary<int, Queue<int>>();
+
+         while(_items.Count > 0)
+         {
+            var inspectedItem = await Inspect();
+            var monkey = GetMonkeyForItem(inspectedItem);
+            if (!tossedItems.ContainsKey(monkey))
+            {
+               tossedItems[monkey] = new Queue<int>();
+            }
+            tossedItems[monkey].Enqueue(inspectedItem);
+         }
+
+         return tossedItems;
+      }
+
+      private async Task<int> Inspect()
+      {
+         var item = _items.Dequeue();
+         item = await GetWorryLevelAfterInspectionAsync(item);
+         item = GetWorryLevelAfterFeelingRelieved(item);
+         return item;
+      }
+
+      private int GetMonkeyForItem(int item)
+      {
+         var testResult = item % _divisorTest == 0;
+         return _testResultMonkey[testResult];
+      }
+
+      private async Task<int> GetWorryLevelAfterInspectionAsync(int item)
+      {
+         try
+         {
+            var result = await CSharpScript.EvaluateAsync<int>(_operation, globals: new Globals { old = item });
+            return result;
+         }
+         catch (Exception)
+         {
+
+            throw;
+         }
+      }
+
+      private int GetWorryLevelAfterFeelingRelieved(int item)
+      {
+         return item / 3;
+      }
+
+      public class Globals
+      {
+         public int old { get; set; }
+      }
+   }
+}
