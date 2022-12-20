ee48bbfc4940fc743d08af71226b41d13e8ed41e
Andrew <blazersand2000@yahoo.com>
2022-12-19 21:48:55 -0500

    Finished part 1 of day 11

--- AdventOfCode/Problems/Day11/Day11.cs
+++ AdventOfCode/Problems/Day11/Day11.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AdventOfCode.Problems.Day11
 {
@@ -11,10 +12,59 @@ namespace AdventOfCode.Problems.Day11
       public void Run()
       {
          var lines = File.ReadAllLines("Problems/Day11/input.txt");
-
+         var monkeyBusiness = GetMonkeyBusiness(lines);
          Console.WriteLine("Part 1:");
+         Console.WriteLine(monkeyBusiness);
 
          Console.WriteLine("Part 2:");
+      }
+
+      private static int GetMonkeyBusiness(string[] lines)
+      {
+         var monkeys = ParseMonkeys(lines);
+
+         for (int i = 0; i < 20; i++)
+         {
+            foreach (var monkey in monkeys)
+            {
+               var thrownItems = monkey.Value.InspectAllItems();
+
+               foreach (var receivingMonkey in thrownItems)
+               {
+                  monkeys[receivingMonkey.Key].CatchItems(receivingMonkey.Value);
+               }
+            }
+         }
+
+         var monkeyBusiness = monkeys.Select(x => x.Value.NumInspections).OrderByDescending(x => x).Take(2).Aggregate((a, b) => a * b);
+         return monkeyBusiness;
+      }
+
+      private static Dictionary<int, Monkey> ParseMonkeys(string[] lines)
+      {
+         var monkeys = new Dictionary<int, Monkey>();
+         var numMonkeys = Math.Ceiling(lines.Length / 7.0);
+
+         for (int i = 0; i < numMonkeys; i++)
+         {
+            var startingIdx = i * 7;
+
+            var items = new List<int>();
+            var operation = string.Empty;
+            var divisorTest = int.MinValue;
+            var toMonkeyOnFalse = int.MinValue;
+            var toMonkeyOnTrue = int.MinValue;
+
+            items = lines[startingIdx + 1].Trim().Split("Starting items: ")[1].Split(", ").Select(x => int.Parse(x)).ToList();
+            operation = lines[startingIdx + 2].Trim().Split("Operation: new = ")[1];
+            divisorTest = int.Parse(lines[startingIdx + 3].Trim().Split("Test: divisible by ")[1]);
+            toMonkeyOnTrue = int.Parse(lines[startingIdx + 4].Trim().Split("If true: throw to monkey ")[1]);
+            toMonkeyOnFalse = int.Parse(lines[startingIdx + 5].Trim().Split("If false: throw to monkey ")[1]);
+
+            monkeys[i] = new Monkey(items, operation, divisorTest, toMonkeyOnFalse, toMonkeyOnTrue);
+         }
+
+         return monkeys;
       }
    }
 }
--- AdventOfCode/Problems/Day11/Monkey.cs
+++ AdventOfCode/Problems/Day11/Monkey.cs
@@ -9,14 +9,16 @@ namespace AdventOfCode.Problems.Day11
    public class Monkey
    {
       private readonly Queue<int> _items;
-      private readonly string _operation;
+      private readonly Func<int, int> _operation;
       private readonly int _divisorTest;
       private readonly Dictionary<bool, int> _testResultMonkey;
+
+      public int NumInspections { get; private set; } = 0;
 
       public Monkey(List<int> items, string operation, int divisorTest, int toMonkeyOnFalse, int toMonkeyOnTrue)
       {
          _items = new Queue<int>(items);
-         _operation = operation;
+         _operation = GetOperation(operation).GetAwaiter().GetResult();
          _divisorTest = divisorTest;
          _testResultMonkey = new Dictionary<bool, int>
          {
@@ -25,13 +27,13 @@ namespace AdventOfCode.Problems.Day11
          };
       }
 
-      public async Task<Dictionary<int, Queue<int>>> InspectAllItems()
+      public Dictionary<int, Queue<int>> InspectAllItems()
       {
          var tossedItems = new Dictionary<int, Queue<int>>();
 
          while(_items.Count > 0)
          {
-            var inspectedItem = await Inspect();
+            var inspectedItem = Inspect();
             var monkey = GetMonkeyForItem(inspectedItem);
             if (!tossedItems.ContainsKey(monkey))
             {
@@ -43,11 +45,20 @@ namespace AdventOfCode.Problems.Day11
          return tossedItems;
       }
 
-      private async Task<int> Inspect()
+      public void CatchItems(Queue<int> items)
+      {
+         foreach (var item in items)
+         {
+            _items.Enqueue(item);
+         }
+      }
+
+      private int Inspect()
       {
          var item = _items.Dequeue();
-         item = await GetWorryLevelAfterInspectionAsync(item);
+         item = GetWorryLevelAfterInspectionAsync(item);
          item = GetWorryLevelAfterFeelingRelieved(item);
+         NumInspections++;
          return item;
       }
 
@@ -57,12 +68,17 @@ namespace AdventOfCode.Problems.Day11
          return _testResultMonkey[testResult];
       }
 
-      private async Task<int> GetWorryLevelAfterInspectionAsync(int item)
+      private int GetWorryLevelAfterInspectionAsync(int item)
+      {
+         return _operation(item);
+      }
+
+      private static async Task<Func<int, int>> GetOperation(string operation)
       {
          try
          {
-            var result = await CSharpScript.EvaluateAsync<int>(_operation, globals: new Globals { old = item });
-            return result;
+            var func = await CSharpScript.EvaluateAsync<Func<int, int>>($"old => {operation}");
+            return func;
          }
          catch (Exception)
          {
