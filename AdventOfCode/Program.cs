fe9eca662da8af225050b03562ef7c587e0d416e
Andrew <blazersand2000@yahoo.com>
2023-12-02 12:33:41 -0500

    Cleanup

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,5 +1,4 @@
-﻿using AdventOfCode.Aoc2022;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
