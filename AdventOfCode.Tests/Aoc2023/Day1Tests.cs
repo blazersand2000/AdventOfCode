1f444d499e40b1f4d5c0b74f0616c9da6c922a0c
Andrew <blazersand2000@yahoo.com>
2023-12-02 16:50:35 -0500

    Finished day 1 part 2

---
+++ AdventOfCode.Tests/Aoc2023/Day1Tests.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode.Aoc2023.Day1;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2023
+{
+   [TestFixture]
+   class Day1Tests
+   {
+      [SetUp]
+      public void Setup()
+      {
+      }
+
+      [Test]
+      public void GetNumericOrSpelledOutCalibrationValues_Tests()
+      {
+         var testData = new[]
+         {
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen"
+         };
+
+         var expected = new[]
+         {
+            29,
+            83,
+            13,
+            24,
+            42,
+            14,
+            76
+         };
+
+         var result = Day1.GetNumericOrSpelledOutCalibrationValues(testData);
+
+         CollectionAssert.AreEqual(expected, result);
+      }
+   }
+}
--- AdventOfCode/Aoc2023/Day1/Day1.cs
+++ AdventOfCode/Aoc2023/Day1/Day1.cs
@@ -6,14 +6,31 @@ namespace AdventOfCode.Aoc2023.Day1
 {
    public class Day1 : Problem
    {
+      private static Dictionary<string, char> spelledOutValues = new()
+      {
+         { "one", '1' },
+         { "two", '2' },
+         { "three", '3' },
+         { "four", '4' },
+         { "five", '5' },
+         { "six", '6' },
+         { "seven", '7' },
+         { "eight", '8' },
+         { "nine", '9' }
+      };
+
       public override void Run()
       {
          string[] lines = ReadInputFile();
 
-         Console.WriteLine(GetCalibrationValues(lines).Sum());
+         Console.WriteLine("Part 1:");
+         Console.WriteLine(GetNumericCalibrationValues(lines).Sum());
+
+         Console.WriteLine("Part 2:");
+         Console.WriteLine(GetNumericOrSpelledOutCalibrationValues(lines).Sum());
       }
 
-      private static IEnumerable<long> GetCalibrationValues(string[] lines)
+      private static IEnumerable<long> GetNumericCalibrationValues(string[] lines)
       {
          return lines.Select(line =>
          {
@@ -31,6 +48,80 @@ namespace AdventOfCode.Aoc2023.Day1
 
             return long.Parse(string.Concat(firstDigit, lastDigit));
          });
+      }
+
+      public static IEnumerable<long> GetNumericOrSpelledOutCalibrationValues(string[] lines)
+      {
+
+         return lines.Select(line =>
+         {
+            var firstDigit = char.MinValue;
+            for (int i = 0; i < line.Length; i++)
+            {
+               if (char.IsDigit(line[i]))
+               {
+                  firstDigit = line[i];
+                  break;
+               }
+               else if (DetectSpelledOutDigit(line, i, out char digit))
+               {
+                  firstDigit = digit;
+                  break;
+               }
+            }
+
+            char lastDigit = char.MinValue;
+            for (int i = line.Length - 1; i >= 0; i--)
+            {
+               if (char.IsDigit(line[i]))
+               {
+                  lastDigit = line[i];
+                  break;
+               }
+               else if (DetectSpelledOutDigit(line, i, out char digit, true))
+               {
+                  lastDigit = digit;
+                  break;
+               }
+            }
+
+            return long.Parse(string.Concat(firstDigit, lastDigit));
+         });
+      }
+
+      // This might be improved by using something like the KMP string searching algorithm.
+      // But for a small number of short strings, this brute force approach is probably fine for the purpose of Advent of Code.
+      private static bool DetectSpelledOutDigit(string line, int startIndex, out char digit, bool reverse = false)
+      {
+         digit = char.MinValue;
+         var i = startIndex;
+         if (startIndex < 0 || startIndex >= line.Length)
+         {
+            return false;
+         }
+
+         // values that wouldn't go out of bounds given our starting index
+         var candidateValues = spelledOutValues.Where(v => !reverse ? startIndex + v.Key.Length <= line.Length : startIndex + 1 - v.Key.Length >= 0);
+         foreach (var spelledOutValue in candidateValues)
+         {
+            if ((!reverse && IsLeftToRightMatch(line, startIndex, spelledOutValue)) || (reverse && IsRightToLeftMatch(line, startIndex, spelledOutValue)))
+            {
+               digit = spelledOutValue.Value;
+               return true;
+            }
+         }
+
+         return false;
+
+         static bool IsLeftToRightMatch(string line, int startIndex, KeyValuePair<string, char> spelledOutValue)
+         {
+            return line.Substring(startIndex, spelledOutValue.Key.Length) == spelledOutValue.Key;
+         }
+
+         static bool IsRightToLeftMatch(string line, int startIndex, KeyValuePair<string, char> spelledOutValue)
+         {
+            return line.Substring(startIndex + 1 - spelledOutValue.Key.Length, spelledOutValue.Key.Length) == spelledOutValue.Key;
+         }
       }
    }
 }
