23bf677f2f46888db0accc5c4f917ceba1c392f9
Andrew <blazersand2000@yahoo.com>
2023-12-02 18:40:32 -0500

    completed day 2 part 1

---
+++ AdventOfCode.Tests/Aoc2023/Day2Tests.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode.Aoc2023.Day2;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2023
+{
+   [TestFixture]
+   class Day2Tests
+   {
+      [SetUp]
+      public void Setup()
+      {
+      }
+
+      [Test]
+      public void GetSumOfPossibleGameIds_Tests()
+      {
+         var testData = new[]
+         {
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+         };
+
+         var expected = 8;
+
+         var result = Day2.GetSumOfPossibleGameIds(testData);
+
+         Assert.AreEqual(expected, result);
+      }
+   }
+}
---
+++ AdventOfCode/Aoc2023/Day2/Day2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Aoc2023.Day2
+{
+   public class Day2 : Problem
+   {
+      public override void Run()
+      {
+         string[] lines = ReadInputFile();
+
+         Console.WriteLine("Part 1:");
+         Console.WriteLine(GetSumOfPossibleGameIds(lines));
+      }
+
+      public static long GetSumOfPossibleGameIds(string[] lines)
+      {
+         var games = ParseGames(lines);
+         var possibleGames = games.Where(game => game.Rounds.Max(round => round.Red) <= 12 && game.Rounds.Max(round => round.Green) <= 13 && game.Rounds.Max(round => round.Blue) <= 14);
+         return possibleGames.Sum(game => game.Id);
+      }
+
+      private static IEnumerable<Game> ParseGames(string[] lines)
+      {
+         return lines.Select(line =>
+         {
+            var gameParts = line.Split(':');
+            var id = int.Parse(gameParts[0].Split(' ')[1]);
+            var rounds = gameParts[1].Split(';').Select(round =>
+            {
+               var roundCounts = round.Split(',', StringSplitOptions.TrimEntries).ToDictionary(s => s.Split(' ')[1], s => int.Parse(s.Split(' ')[0]));
+               return new BagContents(roundCounts.GetValueOrDefault("red", 0), roundCounts.GetValueOrDefault("green", 0), roundCounts.GetValueOrDefault("blue", 0));
+            });
+
+            return new Game(id, rounds.ToList().AsReadOnly());
+         });
+
+      }
+
+      private readonly record struct Game(int Id, IReadOnlyList<BagContents> Rounds);
+
+      private readonly record struct BagContents(int Red, int Green, int Blue);
+
+   }
+}
