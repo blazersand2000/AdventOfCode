2aec9693afe71f27198debde62da02d3fa7fe327
Andrew <blazersand2000@yahoo.com>
2023-12-05 20:32:37 -0500

    Completed day 5 part 1 and have a bad implementation for part 2

--- AdventOfCode/Aoc2023/Day5/Day5.cs
+++ AdventOfCode/Aoc2023/Day5/Day5.cs
@@ -11,12 +11,66 @@ namespace AdventOfCode.Aoc2023.Day5
          var lines = ReadInputFile();
          var almanac = ParseAlmanac(lines);
 
+         var part1 = GetLowestLocationForIndividualSeeds(almanac);
          Console.WriteLine("Part 1:");
+         Console.WriteLine(part1);
 
-         //Console.WriteLine("Part 2:");
+         var part2 = GetLowestLocationForSeedRanges(almanac);
+         Console.WriteLine("Part 2:");
+         Console.WriteLine(part2);
       }
 
-      private static Almanac ParseAlmanac(string[] lines)
+      public static long GetLowestLocationForIndividualSeeds(Almanac almanac)
+      {
+         return GetLowestLocation(almanac.Maps, almanac.Seeds);
+      }
+
+      public static long GetLowestLocationForSeedRanges(Almanac almanac)
+      {
+         var pairs = almanac.Seeds
+            .Select((value, index) => (value, index))
+            .GroupBy(x => x.index / 2)
+            .Select(g => g.Select(x => x.value));
+         var seeds = pairs.SelectMany(pair => LongRange(pair.First(), pair.Last()));
+         return GetLowestLocation(almanac.Maps, seeds);
+      }
+
+      public static IEnumerable<long> LongRange(long start, long count)
+      {
+         for (long i = start; i < start + count; i++)
+         {
+            yield return i;
+         }
+      }
+
+      private static long GetLowestLocation(IEnumerable<IEnumerable<MapRange>> maps, IEnumerable<long> seeds)
+      {
+         return seeds.Min(seed => GetDestination(maps, seed));
+      }
+
+      private static long GetDestination(IEnumerable<IEnumerable<MapRange>> maps, long source)
+      {
+         if (!maps.Any())
+         {
+            return source;
+         }
+
+         var mapRanges = maps.First();
+         var destination = GetMapping(source, mapRanges);
+         return GetDestination(maps.Skip(1), destination);
+      }
+
+      private static long GetMapping(long source, IEnumerable<MapRange> mapRanges)
+      {
+         var mapRange = mapRanges.FirstOrDefault(mr => source >= mr.SourceRangeStart && source < mr.SourceRangeStart + mr.RangeLength);
+         if (mapRange == default)
+         {
+            return source;
+         }
+         return mapRange.DestinationRangeStart + (source - mapRange.SourceRangeStart);
+      }
+
+      public static Almanac ParseAlmanac(string[] lines)
       {
          IEnumerable<long> seeds = lines[0].Split(':', StringSplitOptions.TrimEntries)[1]
             .Split(' ').Select(long.Parse);
@@ -38,8 +92,8 @@ namespace AdventOfCode.Aoc2023.Day5
          return new(seeds, maps);
       }
 
-      private readonly record struct Almanac(IEnumerable<long> Seeds, IEnumerable<IEnumerable<MapRange>> Maps);
+      public readonly record struct Almanac(IEnumerable<long> Seeds, IEnumerable<IEnumerable<MapRange>> Maps);
 
-      private readonly record struct MapRange(long DestinationRangeStart, long SourceRangeStart, long RangeLength);
+      public readonly record struct MapRange(long DestinationRangeStart, long SourceRangeStart, long RangeLength);
    }
 }
