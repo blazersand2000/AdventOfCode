832c4e7f04a756a9be5b496b59e8999a78dc5056
Andrew <blazersand2000@yahoo.com>
2023-12-09 18:37:05 -0500

    Finished day 9

---
+++ AdventOfCode.Tests/Aoc2023/Day9Tests.cs
@@ -0,0 +1,47 @@
+﻿using AdventOfCode.Aoc2023.Day9;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2023
+{
+   [TestFixture]
+   class Day9Tests
+   {
+      private readonly string[] testData = new[]
+      {
+         "0 3 6 9 12 15",
+         "1 3 6 10 15 21",
+         "10 13 16 21 30 45"
+      };
+
+      [SetUp]
+      public void Setup()
+      {
+      }
+
+      [Test]
+      [TestCase(new long[] { 0, 3, 6, 9, 12, 15 }, false, 18)]
+      [TestCase(new long[] { 1, 3, 6, 10, 15, 21, }, false, 28)]
+      [TestCase(new long[] { 10, 13, 16, 21, 30, 45, }, false, 68)]
+      [TestCase(new long[] { 0, 3, 6, 9, 12, 15 }, true, -3)]
+      [TestCase(new long[] { 1, 3, 6, 10, 15, 21, }, true, 0)]
+      [TestCase(new long[] { 10, 13, 16, 21, 30, 45, }, true, 5)]
+      public void ExtrapolateNextValue_Tests(long[] history, bool backwards, long expected)
+      {
+         var result = Day9.ExtrapolateNextValue(history, backwards);
+
+         Assert.AreEqual(expected, result);
+      }
+
+      [Test]
+      [TestCase(false, 114)]
+      [TestCase(true, 2)]
+      public void GetSumOfExtrapolatedValues_Tests(bool backwards, long expected)
+      {
+         var histories = Day9.ParseHistories(testData);
+
+         var result = Day9.GetSumOfExtrapolatedValues(histories, backwards);
+
+         Assert.AreEqual(expected, result);
+      }
+   }
+}
---
+++ AdventOfCode/Aoc2023/Day9/Day9.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Aoc2023.Day9
+{
+   public class Day9 : Problem
+   {
+      public override void Run()
+      {
+         var lines = ReadInputFile();
+         var histories = ParseHistories(lines);
+
+         var part1 = GetSumOfExtrapolatedValues(histories);
+         Console.WriteLine("Part 1:");
+         Console.WriteLine(part1);
+
+         var part2 = GetSumOfExtrapolatedValues(histories, true);
+         Console.WriteLine("Part 2:");
+         Console.WriteLine(part2);
+      }
+
+      public static long GetSumOfExtrapolatedValues(IEnumerable<IEnumerable<long>> histories, bool backwards = false)
+      {
+         return histories.Select(h => ExtrapolateNextValue(h, backwards)).Sum();
+      }
+
+      public static long ExtrapolateNextValue(IEnumerable<long> history, bool backwards)
+      {
+         if (history.All(v => v == 0))
+         {
+            return 0;
+         }
+         var diffs = history.Skip(1).Zip(history, (a, b) => a - b);
+         return backwards
+            ? history.First() - ExtrapolateNextValue(diffs, backwards)
+            : ExtrapolateNextValue(diffs, backwards) + history.Last();
+      }
+
+      public static IEnumerable<IEnumerable<long>> ParseHistories(string[] lines)
+      {
+         return lines.Select(line => line.Split(' ').Select(long.Parse));
+      }
+   }
+}
