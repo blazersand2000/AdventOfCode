937b2718959e0d43e647d91490308aab0666f1e0
Andrew <blazersand2000@yahoo.com>
2023-12-14 21:57:36 -0500

    Completed day 12 part 1

---
+++ AdventOfCode.Tests/Aoc2023/Day12Tests.cs
@@ -0,0 +1,84 @@
+﻿using AdventOfCode.Aoc2023.Day12;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Tests.Aoc2023
+{
+   [TestFixture]
+   class Day12Tests
+   {
+      private readonly string[] testData = new[]
+      {
+         "???.### 1,1,3",
+         ".??..??...?##. 1,1,3",
+         "?#?#?#?#?#?#?#? 1,3,1,6",
+         "????.#...#... 4,1,1",
+         "????.######..#####. 1,6,5",
+         "?###???????? 3,2,1"
+      };
+
+      [SetUp]
+      public void Setup()
+      {
+      }
+
+      [Test]
+      public void ParseRecords_Tests()
+      {
+         var result = Day12.ParseRecords(testData);
+         var expected = new List<Day12.Record>
+         {
+            new("???.###", new[] {1, 1, 3}),
+            new(".??..??...?##.", new[] {1, 1, 3}),
+            new("?#?#?#?#?#?#?#?", new[] {1, 3, 1, 6}),
+            new("????.#...#...", new[] {4, 1, 1}),
+            new("????.######..#####.", new[] {1, 6, 5}),
+            new("?###????????", new[] {3, 2, 1})
+         };
+
+         CollectionAssert.AreEqual(expected, result);
+      }
+
+      [Test]
+      [TestCase("#.#.###", new[] { 1, 1, 3 })]
+      [TestCase(".#...#....###.", new[] { 1, 1, 3 })]
+      [TestCase(".#.###.#.######", new[] { 1, 3, 1, 6 })]
+      [TestCase("####.#...#...", new[] { 4, 1, 1 })]
+      [TestCase("#....######..#####.", new[] { 1, 6, 5 })]
+      [TestCase(".###.##....#", new[] { 3, 2, 1 })]
+      public void UndamagedRecordIsValid_Tests(string springs, int[] damagedGroups)
+      {
+         var record = new Day12.Record(springs, damagedGroups);
+
+         var result = Day12.UndamagedRecordIsValid(record);
+         Assert.IsTrue(result);
+      }
+
+      [Test]
+      [TestCase("???.###", new[] { 1, 1, 3 }, 1)]
+      [TestCase(".??..??...?##.", new[] { 1, 1, 3 }, 4)]
+      [TestCase("?#?#?#?#?#?#?#?", new[] { 1, 3, 1, 6 }, 1)]
+      [TestCase("????.#...#...", new[] { 4, 1, 1 }, 1)]
+      [TestCase("????.######..#####.", new[] { 1, 6, 5 }, 4)]
+      [TestCase("?###????????", new[] { 3, 2, 1 }, 10)]
+      public void GetPossibleArrangements_Tests(string springs, int[] damagedGroups, int expectedCount)
+      {
+         var record = new Day12.Record(springs, damagedGroups);
+
+         var result = Day12.GetPossibleArrangements(record);
+         Assert.AreEqual(expectedCount, result.Count);
+      }
+
+      [Test]
+      [TestCase("???.###", "???.###", false)]
+      [TestCase("###.##.", "???.###", false)]
+      [TestCase("#.#.###", "???.###", true)]
+      public static void CombinationValidForDamagedRecord_Tests(string combination, string damagedRecord, bool expected)
+      {
+         var result = Day12.CombinationValidForDamagedRecord(combination, damagedRecord);
+         Assert.AreEqual(expected, result);
+      }
+   }
+}
---
+++ AdventOfCode/Aoc2023/Day12/Day12.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
+
+namespace AdventOfCode.Aoc2023.Day12
+{
+   public partial class Day12 : Problem
+   {
+      public override void Run()
+      {
+         var lines = ReadInputFile();
+
+         var part1 = Part1(lines);
+         Console.WriteLine("Part 1:");
+         Console.WriteLine(part1);
+
+         // var part2 = Part2(lines);
+         // Console.WriteLine("Part 2:");
+         // Console.WriteLine(part2);
+      }
+
+      public static int Part1(string[] lines)
+      {
+         var records = ParseRecords(lines);
+         var possibleArrangements = records.Select(GetPossibleArrangements).ToArray();
+         return possibleArrangements.Sum(a => a.Count);
+      }
+
+      public static IEnumerable<Record> ParseRecords(string[] lines)
+      {
+         return lines.Select(l =>
+         {
+            var parts = l.Split(" ");
+            var springs = parts[0];
+            var damagedGroups = parts[1].Split(",").Select(int.Parse).ToArray();
+            return new Record(springs, damagedGroups);
+         });
+      }
+
+      public static bool CombinationValidForDamagedRecord(string combination, string damagedRecord)
+      {
+         return combination.Zip(damagedRecord).All(x => x.First != '?' && (x.Second == '?' || x.First == x.Second));
+      }
+
+      public static bool UndamagedRecordIsValid(Record record)
+      {
+         var contiguousGroupsOfDamagedSprings = ContiguousGroupsOfDamagedSpringsRegex().Matches(record.Springs);
+         var groupCounts = contiguousGroupsOfDamagedSprings.Select(m => m.Length).ToArray();
+         return record.DamagedGroups.SequenceEqual(groupCounts);
+      }
+
+      public static List<Record> GetPossibleArrangements(Record record)
+      {
+         var possibleArrangements = new List<Record>();
+         var damagedLocations = record.Springs.Select((s, i) => (s, i)).Where(x => x.s == '?').Select(t => t.i).ToArray();
+         var numCominations = 1 << damagedLocations.Length;
+         for (int i = 0; i < numCominations; i++)
+         {
+            var combination = i;
+            var springs = record.Springs.ToCharArray();
+            for (int j = 0; j < damagedLocations.Length; j++)
+            {
+               var damagedLocation = damagedLocations[j];
+               var damaged = (combination & 1) == 1;
+               if (damaged)
+               {
+                  springs[damagedLocation] = '#';
+               }
+               else
+               {
+                  springs[damagedLocation] = '.';
+               }
+               combination >>= 1;
+            }
+            var candidate = new Record(new string(springs), record.DamagedGroups);
+            if (UndamagedRecordIsValid(candidate))
+            {
+               possibleArrangements.Add(candidate);
+            }
+         }
+
+         return possibleArrangements;
+      }
+
+      public static List<Record> GetPossibleArrangementsPlusPlus(Record record)
+      {
+         var possibleArrangements = new List<Record>();
+         var damagedLocations = record.Springs.Select((s, i) => (s, i)).Where(x => x.s == '?').Select(t => t.i).ToArray();
+         var numCominations = 1 << damagedLocations.Length;
+         for (int i = 0; i < numCominations; i++)
+         {
+            var combination = i;
+            var springs = record.Springs.ToCharArray();
+            for (int j = 0; j < damagedLocations.Length; j++)
+            {
+               var damagedLocation = damagedLocations[j];
+               var damaged = (combination & 1) == 1;
+               if (damaged)
+               {
+                  springs[damagedLocation] = '#';
+               }
+               else
+               {
+                  springs[damagedLocation] = '.';
+               }
+               combination >>= 1;
+            }
+            var candidate = new Record(new string(springs), record.DamagedGroups);
+            if (UndamagedRecordIsValid(candidate))
+            {
+               possibleArrangements.Add(candidate);
+            }
+         }
+
+         return possibleArrangements;
+      }
+
+      public sealed record Record
+      {
+         public string Springs { get; init; }
+         public int[] DamagedGroups { get; init; }
+
+         public Record(string springs, int[] damagedGroups)
+         {
+            Springs = springs;
+            DamagedGroups = damagedGroups;
+         }
+
+         public bool Equals(Record other)
+         {
+            return other != null &&
+                   Springs == other.Springs &&
+                   DamagedGroups.SequenceEqual(other.DamagedGroups);
+         }
+
+         public override int GetHashCode()
+         {
+            return HashCode.Combine(Springs, DamagedGroups);
+         }
+      }
+
+      [GeneratedRegex("#+")]
+      private static partial Regex ContiguousGroupsOfDamagedSpringsRegex();
+
+   }
+}
