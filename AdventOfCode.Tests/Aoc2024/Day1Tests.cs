0589010072954fd6de0eaffeebaef964131582f3
Andrew <blazersand2000@yahoo.com>
2024-12-01 16:54:13 -0500

    Completed 2024 day 1

---
+++ AdventOfCode.Tests/Aoc2024/Day1Tests.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCode.Aoc2024.Day1;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2024
+{
+   [TestFixture]
+   class Day1Tests
+   {
+      string[] testData = new[]
+      {
+         "3   4",
+         "4   3",
+         "2   5",
+         "1   3",
+         "3   9",
+         "3   3"
+      };
+
+      [SetUp]
+      public void Setup()
+      {
+      }
+
+      [Test]
+      public void GetTotalDistance()
+      {
+         var result = Day1.GetTotalDistance(testData);
+
+         Assert.AreEqual(11, result);
+      }
+
+      [Test]
+      public void GetSimilarityScore()
+      {
+         var result = Day1.GetSimilarityScore(testData);
+
+         Assert.AreEqual(31, result);
+      }
+   }
+}
---
+++ AdventOfCode/Aoc2024/Day1/Day1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Aoc2024.Day1
+{
+   public class Day1 : Problem
+   {
+      public override void Run()
+      {
+         string[] lines = ReadInputFile();
+
+         Console.WriteLine("Part 1:");
+         Console.WriteLine(GetTotalDistance(lines));
+
+         Console.WriteLine("Part 2:");
+         Console.WriteLine(GetSimilarityScore(lines));
+      }
+
+      public static int GetTotalDistance(string[] lines)
+      {
+         var (listA, listB) = ExtractLists(lines);
+         var sortedListA = listA.OrderBy(i => i).ToList();
+         var sortedListB = listB.OrderBy(i => i).ToList();
+
+         return sortedListA.Zip(sortedListB, (a, b) => Math.Abs(a - b)).Sum();
+      }
+
+      public static int GetSimilarityScore(string[] lines)
+      {
+         var (listA, listB) = ExtractLists(lines);
+         var listBFrequencies = listB.GroupBy(i => i).ToDictionary(g => g.Key, g => g.Count());
+
+         return listA.Sum(i => listBFrequencies.TryGetValue(i, out var count) ? i * count : 0);
+      }
+
+      private static (List<int> ListA, List<int> ListB) ExtractLists(string[] lines)
+      {
+         var listA = new List<int>();
+         var listB = new List<int>();
+         foreach (var line in lines)
+         {
+            var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            listA.Add(int.Parse(split[0]));
+            listB.Add(int.Parse(split[1]));
+         }
+
+         return (listA, listB);
+      }
+   }
+}
