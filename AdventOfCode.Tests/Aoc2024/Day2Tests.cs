0c1783e72fd6893983c25605542fa33e3367f7aa
Andrew <blazersand2000@yahoo.com>
2024-12-03 00:57:46 -0500

    Finished day 2 part 1

---
+++ AdventOfCode.Tests/Aoc2024/Day2Tests.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode.Aoc2024.Day2;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2024
+{
+   [TestFixture]
+   class Day2Tests
+   {
+      string[] testData = new[]
+      {
+         "7 6 4 2 1",
+         "1 2 7 8 9",
+         "9 7 6 2 1",
+         "1 3 2 4 5",
+         "8 6 4 4 1",
+         "1 3 6 7 9"
+      };
+
+      [SetUp]
+      public void Setup()
+      {
+      }
+
+      [Test]
+      public void GetNumberOfSafeReports()
+      {
+         var result = Day2.GetNumberOfSafeReports(testData);
+
+         Assert.AreEqual(2, result);
+      }
+   }
+}
---
+++ AdventOfCode/Aoc2024/Day2/Day2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Aoc2024.Day2
+{
+   public class Day2 : Problem
+   {
+      public override void Run()
+      {
+         string[] lines = ReadInputFile();
+
+         Console.WriteLine("Part 1:");
+         Console.WriteLine(GetNumberOfSafeReports(lines));
+
+         //Console.WriteLine("Part 2:");
+         //Console.WriteLine(GetSimilarityScore(lines));
+      }
+
+      public static int GetNumberOfSafeReports(string[] lines)
+      {
+         var reports = lines.Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)));
+
+         return reports.Count(report =>
+         {
+            var direction = 0;
+            foreach (var (a, b) in GetPairs(report.ToList()))
+            {
+               var difference = b - a;
+               if (Math.Abs(difference) < 1 || Math.Abs(difference) > 3)
+               {
+                  return false;
+               }
+               if (direction == 0)
+               {
+                  direction = b - a;
+                  continue;
+               }
+               if (direction < 0 && difference > 0)
+               {
+                  return false;
+               }
+               if (direction > 0 && difference < 0)
+               {
+                  return false;
+               }
+            }
+            return true;
+         });
+      }
+
+      public static IEnumerable<(int, int)> GetPairs(List<int> list)
+      {
+         for (int i = 0; i < list.Count() - 1; i++)
+         {
+            yield return (list[i], list[i + 1]);
+         }
+      }
+   }
+}
