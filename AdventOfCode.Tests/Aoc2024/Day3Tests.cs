73474ee60642311dc82c82308289f16a1be4ee7e
Andrew <blazersand2000@yahoo.com>
2024-12-03 23:53:09 -0500

    Finished day 3 part 1

---
+++ AdventOfCode.Tests/Aoc2024/Day3Tests.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode.Aoc2024.Day3;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2024
+{
+   [TestFixture]
+   class Day3Tests
+   {
+      string[] testData = ["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"];
+
+      [SetUp]
+      public void Setup()
+      {
+      }
+
+      [Test]
+      public void GetSumOfMultiplicationResults()
+      {
+         var result = Day3.GetSumOfMultiplicationResults(testData);
+
+         Assert.AreEqual(161, result);
+      }
+
+      // [Test]
+      // public void GetNumberOfSafeReportUsingDampener()
+      // {
+      //    var result = Day2.GetNumberOfSafeReportsUsingDampener(testData);
+
+      //    Assert.AreEqual(4, result);
+      // }
+   }
+}
---
+++ AdventOfCode/Aoc2024/Day3/Day3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Aoc2024.Day3
+{
+   public class Day3 : Problem
+   {
+      public override void Run()
+      {
+         string[] lines = ReadInputFile();
+
+         Console.WriteLine("Part 1:");
+         Console.WriteLine(GetSumOfMultiplicationResults(lines));
+
+         // Console.WriteLine("Part 2:");
+         // Console.WriteLine(GetNumberOfSafeReportsUsingDampener(lines));
+      }
+
+      public static int GetSumOfMultiplicationResults(string[] lines)
+      {
+         var corruptedMemory = string.Join(string.Empty, lines);
+         var pattern = @"mul\(\d{1,3},\d{1,3}\)";
+         var pairs = Regex.Matches(corruptedMemory, pattern).Select(x => x.Value[4..^1].Split([',']));
+         return pairs.Sum(p => int.Parse(p[0]) * int.Parse(p[1]));
+      }
+   }
+}
