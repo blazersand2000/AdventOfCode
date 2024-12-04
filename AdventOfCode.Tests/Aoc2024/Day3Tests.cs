88f54f61c424191f5b5b5e17d05850c715bb7077
Andrew <blazersand2000@yahoo.com>
2024-12-04 00:32:13 -0500

    Finished day 3

--- AdventOfCode.Tests/Aoc2024/Day3Tests.cs
+++ AdventOfCode.Tests/Aoc2024/Day3Tests.cs
@@ -6,8 +6,6 @@ namespace AdventOfCode.Tests.Aoc2024
    [TestFixture]
    class Day3Tests
    {
-      string[] testData = ["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"];
-
       [SetUp]
       public void Setup()
       {
@@ -16,17 +14,21 @@ namespace AdventOfCode.Tests.Aoc2024
       [Test]
       public void GetSumOfMultiplicationResults()
       {
+         string[] testData = ["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"];
+
          var result = Day3.GetSumOfMultiplicationResults(testData);
 
          Assert.AreEqual(161, result);
       }
 
-      // [Test]
-      // public void GetNumberOfSafeReportUsingDampener()
-      // {
-      //    var result = Day2.GetNumberOfSafeReportsUsingDampener(testData);
+      [Test]
+      public void GetSumOfEnabledMultiplicationResults()
+      {
+         string[] testData = ["xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"];
 
-      //    Assert.AreEqual(4, result);
-      // }
+         var result = Day3.GetSumOfEnabledMultiplicationResults(testData);
+
+         Assert.AreEqual(48, result);
+      }
    }
 }
--- AdventOfCode/Aoc2024/Day3/Day3.cs
+++ AdventOfCode/Aoc2024/Day3/Day3.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -14,16 +13,29 @@ namespace AdventOfCode.Aoc2024.Day3
          Console.WriteLine("Part 1:");
          Console.WriteLine(GetSumOfMultiplicationResults(lines));
 
-         // Console.WriteLine("Part 2:");
-         // Console.WriteLine(GetNumberOfSafeReportsUsingDampener(lines));
+         Console.WriteLine("Part 2:");
+         Console.WriteLine(GetSumOfEnabledMultiplicationResults(lines));
       }
 
       public static int GetSumOfMultiplicationResults(string[] lines)
       {
          var corruptedMemory = string.Join(string.Empty, lines);
+         return GetSumOfMultiplicationResults(corruptedMemory);
+      }
+
+      private static int GetSumOfMultiplicationResults(string corruptedMemory)
+      {
          var pattern = @"mul\(\d{1,3},\d{1,3}\)";
          var pairs = Regex.Matches(corruptedMemory, pattern).Select(x => x.Value[4..^1].Split([',']));
          return pairs.Sum(p => int.Parse(p[0]) * int.Parse(p[1]));
+      }
+
+      public static int GetSumOfEnabledMultiplicationResults(string[] lines)
+      {
+         var corruptedMemory = string.Join(string.Empty, lines);
+         var pattern = @"don't\(\).*?do\(\)";
+         var filteredCorruptedMemory = Regex.Replace(corruptedMemory, pattern, string.Empty);
+         return GetSumOfMultiplicationResults(filteredCorruptedMemory);
       }
    }
 }
