304c68cf55d0c59e950b7301e143bec89be80471
Andrew <blazersand2000@yahoo.com>
2024-12-08 17:51:41 -0500

    Finished day 7

---
+++ AdventOfCode.Tests/Aoc2024/Day7Tests.cs
@@ -0,0 +1,43 @@
+﻿using AdventOfCode.Aoc2024.Day7;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2024
+{
+   [TestFixture]
+   class Day7Tests
+   {
+      private readonly string[] testData =
+      {
+         "190: 10 19",
+         "3267: 81 40 27",
+         "83: 17 5",
+         "156: 15 6",
+         "7290: 6 8 6 15",
+         "161011: 16 10 13",
+         "192: 17 8 14",
+         "21037: 9 7 18 13",
+         "292: 11 6 16 20"
+      };
+
+      [SetUp]
+      public void Setup()
+      {
+      }
+
+      [Test]
+      public void GetCalibrationResult()
+      {
+         var result = Day7.GetCalibrationResult(testData);
+
+         Assert.AreEqual(3749, result);
+      }
+
+      [Test]
+      public void GetCalibrationResultIncludingConcatenation()
+      {
+         var result = Day7.GetCalibrationResultIncludingConcatenation(testData);
+
+         Assert.AreEqual(11387, result);
+      }
+   }
+}
---
+++ AdventOfCode/Aoc2024/Day7/Day7.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Aoc2024.Day7
+{
+   public class Day7 : Problem
+   {
+      public override void Run()
+      {
+         string[] lines = ReadInputFile();
+
+         Console.WriteLine("Part 1:");
+         Console.WriteLine(GetCalibrationResult(lines));
+
+         Console.WriteLine("Part 2:");
+         Console.WriteLine(GetCalibrationResultIncludingConcatenation(lines));
+      }
+
+      public static long GetCalibrationResult(string[] lines)
+      {
+         var equations = ParseEquations(lines);
+         return equations.Where(e => CanBeMadeTrue(e, [Add, Multiply])).Sum(e => e.TestValue);
+      }
+
+      public static long GetCalibrationResultIncludingConcatenation(string[] lines)
+      {
+         var equations = ParseEquations(lines);
+         return equations.Where(e => CanBeMadeTrue(e, [Add, Multiply, Concatenate])).Sum(e => e.TestValue);
+      }
+
+      private static bool CanBeMadeTrue(Equation equation, List<Func<long, long, long>> operations)
+      {
+         return NumberOfWaysCanBeMadeTrue(equation, operations) > 0;
+      }
+
+      private static int NumberOfWaysCanBeMadeTrue(Equation equation, List<Func<long, long, long>> operations)
+      {
+         if (equation.Values[0] > equation.TestValue)
+         {
+            return 0;
+         }
+         if (equation.Values.Count == 1)
+         {
+            return equation.Values[0] == equation.TestValue ? 1 : 0;
+         }
+
+         return operations.Sum(operation =>
+         {
+            var result = operation(equation.Values[0], equation.Values[1]);
+            var nextEquation = new Equation(equation.TestValue, [result, .. equation.Values[2..]]);
+
+            return NumberOfWaysCanBeMadeTrue(nextEquation, operations);
+         });
+      }
+
+      private static long Add(long a, long b) => a + b;
+
+      private static long Multiply(long a, long b) => a * b;
+
+      private static long Concatenate(long a, long b) => long.Parse($"{a}{b}");
+
+      private static List<Equation> ParseEquations(string[] lines)
+      {
+         return lines.Select(line =>
+         {
+            var parts = line.Split(new[] { ':', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+               .Select(part => long.Parse(part))
+               .ToList();
+            return new Equation(parts[0], parts[1..]);
+         }).ToList();
+      }
+
+      public record Equation(long TestValue, List<long> Values);
+   }
+}
