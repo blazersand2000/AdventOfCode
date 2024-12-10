5a7901cb9eaaee1487fccac78a1ce3bbb72936d1
Andrew <blazersand2000@yahoo.com>
2024-12-10 02:26:42 -0500

    Finished day 9

---
+++ AdventOfCode.Tests/Aoc2024/Day9Tests.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using AdventOfCode.Aoc2024.Day9;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2024
+{
+   [TestFixture]
+   class Day9Tests
+   {
+      private readonly string testData = "2333133121414131402";
+
+      [SetUp]
+      public void Setup()
+      {
+      }
+
+      [Test]
+      public void InitializeBlocks()
+      {
+         var result = Day9.InitializeBlocksFromMap(testData);
+
+         List<int?> expected = [0, 0, null, null, null, 1, 1, 1, null, null, null, 2, null, null, null, 3, 3, 3,
+            null, 4, 4, null, 5, 5, 5, 5, null, 6, 6, 6, 6, null, 7, 7, 7, null, 8, 8, 8, 8, 9, 9];
+
+         CollectionAssert.AreEqual(expected, result);
+      }
+
+      [Test]
+      public void RearrangeBlocks()
+      {
+         List<int?> blocks = [0, 0, null, null, null, 1, 1, 1, null, null, null, 2, null, null, null, 3, 3, 3,
+            null, 4, 4, null, 5, 5, 5, 5, null, 6, 6, 6, 6, null, 7, 7, 7, null, 8, 8, 8, 8, 9, 9];
+
+         List<int?> expected = [0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7, 3, 3, 3, 6, 4, 4, 6, 5, 5, 5, 5, 6, 6,
+            null, null, null, null, null, null, null, null, null, null, null, null, null, null];
+
+         var result = Day9.RearrangeBlocks(blocks);
+
+         CollectionAssert.AreEqual(expected, result);
+      }
+
+      [Test]
+      public void RearrangeEntireFiles()
+      {
+         List<int?> blocks = [0, 0, null, null, null, 1, 1, 1, null, null, null, 2, null, null, null, 3, 3, 3,
+            null, 4, 4, null, 5, 5, 5, 5, null, 6, 6, 6, 6, null, 7, 7, 7, null, 8, 8, 8, 8, 9, 9];
+
+         List<int?> expected = [0, 0, 9, 9, 2, 1, 1, 1, 7, 7, 7, null, 4, 4, null, 3, 3, 3, null, null, null, null,
+         5, 5, 5, 5, null, 6, 6, 6, 6, null, null, null, null, null, 8, 8, 8, 8, null, null];
+
+         var result = Day9.RearrangeEntireFiles(blocks);
+
+         CollectionAssert.AreEqual(expected, result);
+      }
+
+      [Test]
+      public void GetFileSystemChecksum()
+      {
+         var result = Day9.GetFileSystemChecksum([testData]);
+
+         Assert.AreEqual(1928, result);
+      }
+
+      [Test]
+      public void GetFileSystemChecksumAfterRearrangingEntireFiles()
+      {
+         var result = Day9.GetFileSystemChecksumAfterRearrangingEntireFiles([testData]);
+
+         Assert.AreEqual(2858, result);
+      }
+   }
+}
---
+++ AdventOfCode/Aoc2024/Day9/Day9.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+
+namespace AdventOfCode.Aoc2024.Day9
+{
+   public class Day9 : Problem
+   {
+      public override void Run()
+      {
+         string[] lines = ReadInputFile();
+
+         Console.WriteLine("Part 1:");
+         Console.WriteLine(GetFileSystemChecksum(lines));
+
+         Console.WriteLine("Part 2:");
+         Console.WriteLine(GetFileSystemChecksumAfterRearrangingEntireFiles(lines));
+
+      }
+
+      public static long GetFileSystemChecksum(string[] lines)
+      {
+         var blocks = InitializeBlocksFromMap(lines[0]);
+         var rearranged = RearrangeBlocks(blocks.ToList());
+         var checksum = CalculateChecksum(rearranged);
+         return checksum;
+      }
+
+      public static long GetFileSystemChecksumAfterRearrangingEntireFiles(string[] lines)
+      {
+         var blocks = InitializeBlocksFromMap(lines[0]);
+         var rearranged = RearrangeEntireFiles(blocks.ToList());
+         var checksum = CalculateChecksum(rearranged);
+         return checksum;
+      }
+
+      public static List<int?> RearrangeBlocks(List<int?> original)
+      {
+         var blocks = original.ToList();
+         var to = 0;
+         var from = blocks.Count - 1;
+
+         while (from > to)
+         {
+            if (blocks[to] != null)
+            {
+               to++;
+               continue;
+            }
+            if (blocks[from] == null)
+            {
+               from--;
+               continue;
+            }
+
+            blocks[to] = blocks[from];
+            blocks[from] = null;
+            to++;
+            from--;
+         }
+
+         return blocks;
+      }
+
+      public static List<int?> RearrangeEntireFiles(List<int?> original)
+      {
+         // I feel like I should have tried this using the map form.
+
+         var blocks = original.ToList();
+
+         for (int i = maxFileId(); i >= 0; i--)
+         {
+            var file = findFile(i);
+            var to = firstIndexOfFreeSpace(file.Length);
+            if (to > -1 && to < file.Index)
+            {
+               move(file.Index, to, file.Length);
+            }
+         }
+
+         return blocks;
+
+         void move(int from, int to, int blockSize)
+         {
+            for (int i = 0; i < blockSize; i++)
+            {
+               blocks[to + i] = blocks[from + i];
+               blocks[from + i] = null;
+            }
+         }
+
+         int firstIndexOfFreeSpace(int blockSize)
+         {
+            for (int i = 0, start = int.MinValue; i < blocks.Count; i++)
+            {
+               if (blocks[i] != null)
+               {
+                  start = int.MinValue;
+               }
+               else if (start == int.MinValue)
+               {
+                  start = i;
+               }
+               if (i - start + 1 == blockSize)
+               {
+                  return start;
+               }
+            }
+
+            return -1;
+         }
+
+         (int Index, int Length) findFile(int fileId)
+         {
+            var index = blocks.IndexOf(fileId);
+            var length = blocks.Where(b => b == fileId).Count();
+            return (index, length);
+         }
+
+         int maxFileId() => blocks.Select(b => b ?? 0).Max();
+      }
+
+      public static IEnumerable<int?> InitializeBlocksFromMap(string diskMap)
+      {
+         for (int i = 0; i < diskMap.Length; i++)
+         {
+            var count = int.Parse(diskMap[i].ToString());
+            if (i % 2 == 0)
+            {
+               foreach (var item in Enumerable.Repeat(i / 2, count))
+               {
+                  yield return item;
+               }
+            }
+            else
+            {
+               foreach (var item in Enumerable.Repeat((int?)null, count))
+               {
+                  yield return item;
+               }
+            }
+         }
+      }
+
+      private static long CalculateChecksum(List<int?> blocks)
+      {
+         return blocks.Select((id, position) => (long)position * (id ?? 0)).Sum();
+      }
+   }
+}
