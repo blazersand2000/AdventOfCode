bee6159437bd0f2a10ac45ce1a9c6ea35cfd43d6
Andrew <blazersand2000@yahoo.com>
2024-12-16 23:35:18 -0500

    Finished day 13

---
+++ AdventOfCode.Tests/Aoc2024/Day13Tests.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using AdventOfCode.Aoc2024.Day13;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2024
+{
+   [TestFixture]
+   class Day13Tests
+   {
+      private readonly string[] testData =
+      [
+         "Button A: X+94, Y+34",
+         "Button B: X+22, Y+67",
+         "Prize: X=8400, Y=5400",
+         "",
+         "Button A: X+26, Y+66",
+         "Button B: X+67, Y+21",
+         "Prize: X=12748, Y=12176",
+         "",
+         "Button A: X+17, Y+86",
+         "Button B: X+84, Y+37",
+         "Prize: X=7870, Y=6450",
+         "",
+         "Button A: X+69, Y+23",
+         "Button B: X+27, Y+71",
+         "Prize: X=18641, Y=10279"
+      ];
+
+      [SetUp]
+      public void Setup()
+      {
+      }
+
+      [Test]
+      [TestCase(94, 22, -8400, 34, 67, -5400, 80, 40)]
+      //[TestCase(26, 67, -12748, 66, 21, -12176, 80, 40)]
+      public void SolveSystem(int a1, int b1, int c1, int a2, int b2, int c2, double expectedX, double expectedY)
+      {
+         var first = new LinearEquation(a1, b1, c1);
+         var second = new LinearEquation(a2, b2, c2);
+
+         var result = Day13.SolveSystem(first, second);
+
+         Assert.AreEqual(expectedX, result.X);
+         Assert.AreEqual(expectedY, result.Y);
+      }
+
+      [Test]
+      public void GetFewestTokensSpentToWinAllPrizes()
+      {
+         var result = Day13.GetFewestTokensSpentToWinAllPrizes(testData);
+
+         Assert.AreEqual(480, result);
+      }
+
+      // [Test]
+      // public void GetFewestTokensSpentToWinAllPrizes_PrizeOffsetByTenTrillion()
+      // {
+      //    var result = Day13.GetFewestTokensSpentToWinAllPrizes(testData, 10_000_000_000_000);
+
+      //    Assert.AreEqual(480, result);
+      // }
+   }
+}
---
+++ AdventOfCode/Aoc2024/Day13/Day13.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Aoc2024.Day13
+{
+   public class Day13 : Problem
+   {
+      public override void Run()
+      {
+         string[] lines = ReadInputFile();
+
+         Console.WriteLine("Part 1:");
+         Console.WriteLine(GetFewestTokensSpentToWinAllPrizes(lines));
+
+         Console.WriteLine("Part 2:");
+         Console.WriteLine(GetFewestTokensSpentToWinAllPrizes(lines, 10_000_000_000_000));
+      }
+
+      public static long GetFewestTokensSpentToWinAllPrizes(string[] lines, long prizeOffset = 0L)
+      {
+         var clawMachines = ExtractClawMachines(lines);
+
+         var tokens = clawMachines.Sum(clawMachine =>
+         {
+            var firstEquation = new LinearEquation(clawMachine.A.X, clawMachine.B.X, -(clawMachine.Prize.X + prizeOffset));
+            var secondEquation = new LinearEquation(clawMachine.A.Y, clawMachine.B.Y, -(clawMachine.Prize.Y + prizeOffset));
+            var (buttonAPushes, buttonBPushes) = SolveSystem(firstEquation, secondEquation);
+
+            if (!IsInteger(buttonAPushes) || !IsInteger(buttonBPushes))
+            {
+               return 0;
+            }
+
+            return 3 * (long)buttonAPushes + 1 * (long)buttonBPushes;
+         });
+
+         return tokens;
+      }
+
+      public static (double X, double Y) SolveSystem(LinearEquation first, LinearEquation second)
+      {
+         var x = (double)(first.B * -second.C - second.B * -first.C) / (second.A * first.B - second.B * first.A);
+         var y = (double)(first.A * -second.C - second.A * -first.C) / (second.B * first.A - second.A * first.B);
+
+         return (x, y);
+      }
+
+      private static bool IsInteger(double value)
+      {
+         return value == Math.Floor(value);
+      }
+
+      private static List<ClawMachine> ExtractClawMachines(string[] lines)
+      {
+         return lines.Chunk(4).Select(chunk =>
+         {
+            var buttonA = extractButton(chunk[0]);
+            var buttonB = extractButton(chunk[1]);
+            var prize = extractPrize(chunk[2]);
+            return new ClawMachine(buttonA, buttonB, prize);
+         }).ToList();
+
+         Button extractButton(string line)
+         {
+            var parts = line.Split();
+            var x = long.Parse(parts[2][2..^1]);
+            var y = long.Parse(parts[3][2..]);
+
+            return new Button(x, y);
+         }
+
+         Prize extractPrize(string line)
+         {
+            var parts = line.Split();
+            var x = long.Parse(parts[1][2..^1]);
+            var y = long.Parse(parts[2][2..]);
+
+            return new Prize(x, y);
+         }
+      }
+
+   }
+
+   /// <summary>
+   /// ax + by + c = 0
+   /// </summary>
+   public record struct LinearEquation(long A, long B, long C);
+
+   public record struct Button(long X, long Y);
+
+   public record struct Prize(long X, long Y);
+
+   public record struct ClawMachine(Button A, Button B, Prize Prize);
+}
