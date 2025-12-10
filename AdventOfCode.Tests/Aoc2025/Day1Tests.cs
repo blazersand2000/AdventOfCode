9215589451207d4586ee650eb38800b93653408d
Andrew <blazersand2000@yahoo.com>
2025-12-09 20:37:48 -0500

    Finished 2025 day 1

---
+++ AdventOfCode.Tests/Aoc2025/Day1Tests.cs
@@ -0,0 +1,54 @@
+﻿using AdventOfCode.Aoc2025.Day1;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2025;
+
+[TestFixture]
+class Day1Tests
+{
+   readonly string[] testData =
+   [
+      "L68",
+      "L30",
+      "R48",
+      "L5",
+      "R60",
+      "L55",
+      "L1",
+      "L99",
+      "R14",
+      "L82"
+   ];
+
+   [SetUp]
+   public void Setup()
+   {
+   }
+
+   [Test]
+   public void GetPassword()
+   {
+      var result = Day1.GetPassword(testData);
+
+      Assert.AreEqual(3, result);
+   }
+
+   [Test]
+   public void GetPasswordUsingMethod0x434C49434B()
+   {
+      var result = Day1.GetPasswordUsingMethod0x434C49434B(testData);
+
+      Assert.AreEqual(6, result);
+   }
+
+   [Test]
+   public void GetPasswordUsingMethod0x434C49434B_LandingOnZeroRotatingNegative()
+   {
+      string[] data = ["L50", "L5"];
+
+      var result = Day1.GetPasswordUsingMethod0x434C49434B(data);
+
+      Assert.AreEqual(1, result);
+   }
+
+}
---
+++ AdventOfCode/Aoc2025/Day1/Day1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace AdventOfCode.Aoc2025.Day1;
+
+public class Day1 : Problem
+{
+   private const int DIAL_LENGTH = 100;
+
+   public override void Run()
+   {
+      string[] lines = ReadInputFile();
+
+      Console.WriteLine("Part 1:");
+      Console.WriteLine(GetPassword(lines));
+
+      Console.WriteLine("Part 2:");
+      Console.WriteLine(GetPasswordUsingMethod0x434C49434B(lines));
+   }
+
+   public static int GetPassword(string[] lines)
+   {
+      var rotations = GetRotations(lines);
+
+      var numberOfTimesEndingAtZero = 0;
+      uint current = 50;
+
+      foreach (var rotation in rotations)
+      {
+         current = Rotate(current, rotation);
+         if (current == 0)
+         {
+            numberOfTimesEndingAtZero++;
+         }
+      }
+
+      return numberOfTimesEndingAtZero;
+   }
+
+   // There's gotta be a better way, but I gave up
+   public static int GetPasswordUsingMethod0x434C49434B(string[] lines)
+   {
+      var rotations = GetRotations(lines);
+
+      uint numberOfTimesPointingAtZero = 0;
+      int current = 50;
+
+      foreach (var rotation in rotations)
+      {
+         var clicks = Math.Abs(rotation);
+         for (int i = 0; i < clicks; i++)
+         {
+            current += rotation < 0 ? -1 : 1;
+            if (current == DIAL_LENGTH)
+            {
+               current = 0;
+            }
+            else if (current == -1)
+            {
+               current = DIAL_LENGTH - 1;
+            }
+
+            if (current == 0)
+            {
+               numberOfTimesPointingAtZero++;
+            }
+         }
+      }
+
+      return (int)numberOfTimesPointingAtZero;
+   }
+
+   private static IEnumerable<int> GetRotations(string[] lines)
+   {
+      return lines.Select(line => int.Parse(line[1..]) * (line[0] == 'L' ? -1 : 1));
+   }
+
+   private static uint Rotate(uint current, int rotation)
+   {
+      var rotatedPossiblyNegative = (current + rotation) % DIAL_LENGTH;
+      return rotatedPossiblyNegative < 0 ? (uint)(rotatedPossiblyNegative + DIAL_LENGTH) : (uint)rotatedPossiblyNegative;
+   }
+
+   // Only supports positive rotations
+   private static uint GetNumberOfTimesPointingAtZero(uint current, uint rotation)
+   {
+      return (current + rotation) / DIAL_LENGTH;
+   }
+}
