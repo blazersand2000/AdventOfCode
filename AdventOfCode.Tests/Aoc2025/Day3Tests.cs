80f63bf2d09ba819408ed965b8990c5116350175
Andrew <blazersand2000@yahoo.com>
2025-12-10 01:48:30 -0500

    Finished day 3 part 1

---
+++ AdventOfCode.Tests/Aoc2025/Day3Tests.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode.Aoc2025.Day3;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2025;
+
+[TestFixture]
+class Day3Tests
+{
+   readonly string[] testData =
+   [
+      "987654321111111",
+      "811111111111119",
+      "234234234234278",
+      "818181911112111"
+   ];
+
+   [SetUp]
+   public void Setup()
+   {
+   }
+
+   [Test]
+   public void GetSumOfInvalidIds()
+   {
+      var result = Day3.GetTotalOutputJoltage(testData);
+
+      Assert.AreEqual(357, result);
+   }
+}
---
+++ AdventOfCode/Aoc2025/Day3/Day3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace AdventOfCode.Aoc2025.Day3;
+
+public class Day3 : Problem
+{
+   public override void Run()
+   {
+      string[] lines = ReadInputFile();
+
+      Console.WriteLine("Part 1:");
+      Console.WriteLine(GetTotalOutputJoltage(lines));
+
+      // Console.WriteLine("Part 2:");
+      // Console.WriteLine(GetSumOfInvalidIdsUsingNewRules(lines[0]));
+   }
+
+   public static long GetTotalOutputJoltage(string[] banks)
+   {
+      return banks.Sum(GetJoltage);
+   }
+
+   private static int GetJoltage(string bank)
+   {
+      var left = bank[0..^1].Max();
+      var right = bank[(bank.IndexOf(left) + 1)..].Max();
+
+      return int.Parse($"{left}{right}");
+   }
+
+}
