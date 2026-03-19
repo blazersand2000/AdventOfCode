a898c18f0f69dac1ba85a6de2f66965777ac345c
Andrew <blazersand2000@yahoo.com>
2026-03-18 20:41:12 -0400

    Finished day 5

---
+++ AdventOfCode.Tests/Aoc2025/Day5Tests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using AdventOfCode.Aoc2025.Day5;
+using NUnit.Framework;
+using Range = AdventOfCode.Aoc2025.Day5.Day5.Range;
+
+namespace AdventOfCode.Tests.Aoc2025;
+
+[TestFixture]
+class Day5Tests
+{
+   readonly string[] testData =
+   [
+      "3-5",
+      "10-14",
+      "16-20",
+      "12-18",
+      "",
+      "1",
+      "5",
+      "8",
+      "11",
+      "17",
+      "32"
+   ];
+
+   [SetUp]
+   public void Setup()
+   {
+   }
+
+   [Test]
+   public void GetCountOfFreshIngredients()
+   {
+      var result = Day5.GetCountOfAvailableFreshIngredients(testData);
+
+      Assert.AreEqual(3, result);
+   }
+
+   [Test]
+   public void GetCountOfIngredientsConsideredToBeFresh()
+   {
+      var result = Day5.GetCountOfIngredientsConsideredToBeFresh(testData);
+
+      Assert.AreEqual(14, result);
+   }
+
+   [Test]
+   [TestCase(10, 20, 30, 40, ExpectedResult = false)]
+   [TestCase(10, 20, 20, 30, ExpectedResult = true)]
+   [TestCase(10, 30, 20, 40, ExpectedResult = true)]
+   [TestCase(20, 40, 10, 30, ExpectedResult = true)]
+   [TestCase(20, 30, 10, 20, ExpectedResult = true)]
+   [TestCase(30, 40, 10, 20, ExpectedResult = false)]
+   [TestCase(10, 40, 20, 30, ExpectedResult = true)]
+   [TestCase(20, 30, 10, 40, ExpectedResult = true)]
+   public bool RangesOverlap(long aStart, long aEnd, long bStart, long bEnd)
+   {
+      return Day5.RangesOverlap(new Range(aStart, aEnd), new Range(bStart, bEnd));
+   }
+}
---
+++ AdventOfCode/Aoc2025/Day5/Day5.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace AdventOfCode.Aoc2025.Day5;
+
+public class Day5 : Problem
+{
+   public override void Run()
+   {
+      string[] lines = ReadInputFile();
+
+      Console.WriteLine("Part 1:");
+      Console.WriteLine(GetCountOfAvailableFreshIngredients(lines));
+
+      Console.WriteLine("Part 2:");
+      Console.WriteLine(GetCountOfIngredientsConsideredToBeFresh(lines));
+   }
+
+   public static long GetCountOfAvailableFreshIngredients(string[] lines)
+   {
+      var freshIngredientRanges = GetRanges(lines);
+      var availableIngredients = GetIngredientIds(lines);
+
+      return availableIngredients.Count(ingredient => freshIngredientRanges.Any(range => RangeContains(range, ingredient)));
+   }
+
+   public static long GetCountOfIngredientsConsideredToBeFresh(string[] lines)
+   {
+      var freshIngredientRanges = GetRanges(lines);
+      var distinctRanges = new List<Range>();
+
+      foreach (var range in freshIngredientRanges)
+      {
+         var existingOverlaps = distinctRanges.Where(dr => RangesOverlap(range, dr)).ToList();
+         var newCombinedRange = MergeRanges([range, .. existingOverlaps]);
+         distinctRanges = distinctRanges.Except(existingOverlaps).ToList();
+         distinctRanges.Add(newCombinedRange);
+      }
+
+      return distinctRanges.Sum(dr => dr.End - dr.Start + 1);
+   }
+
+   private static List<Range> GetRanges(string[] lines)
+   {
+      return lines.TakeWhile(line => line != string.Empty).Select(ParseRange).ToList();
+   }
+
+   private static List<long> GetIngredientIds(string[] lines)
+   {
+      return lines.SkipWhile(line => line.Contains('-') || line == string.Empty).Select(long.Parse).ToList();
+   }
+
+   private static Range ParseRange(string range)
+   {
+      var parts = range.Split('-');
+      var start = long.Parse(parts[0]);
+      var end = long.Parse(parts[1]);
+      return new Range(start, end);
+   }
+
+   private static bool RangeContains(Range range, long value)
+   {
+      return value >= range.Start && value <= range.End;
+   }
+
+   public static bool RangesOverlap(Range a, Range b)
+   {
+      return a.End >= b.Start && a.Start <= b.End;
+   }
+
+   private static Range MergeRanges(List<Range> ranges)
+   {
+      var start = ranges.Min(r => r.Start);
+      var end = ranges.Max(r => r.End);
+
+      return new Range(start, end);
+   }
+
+   public readonly record struct Range(long Start, long End);
+}
