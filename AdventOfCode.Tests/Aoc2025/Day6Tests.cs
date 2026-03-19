d377078f2334b5859173a2222b018b9e8b552014
Andrew <blazersand2000@yahoo.com>
2026-03-18 21:43:43 -0400

    Finished day 6 part 1

---
+++ AdventOfCode.Tests/Aoc2025/Day6Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AdventOfCode.Aoc2025.Day6;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2025;
+
+[TestFixture]
+class Day6Tests
+{
+   readonly string[] testData =
+   [
+      "123 328  51 64 ",
+      " 45 64  387 23 ",
+      "  6 98  215 314",
+      "*   +   *   +  ",
+   ];
+
+   [SetUp]
+   public void Setup()
+   {
+   }
+
+   [Test]
+   public void GetGrandTotal()
+   {
+      var result = Day6.GetGrandTotal(testData);
+
+      Assert.AreEqual(4277556, result);
+   }
+
+   // [Test]
+   // public void GetCountOfIngredientsConsideredToBeFresh()
+   // {
+   //    var result = Day5.GetCountOfIngredientsConsideredToBeFresh(testData);
+
+   //    Assert.AreEqual(14, result);
+   // }
+}
---
+++ AdventOfCode/Aoc2025/Day6/Day6.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace AdventOfCode.Aoc2025.Day6;
+
+public class Day6 : Problem
+{
+   public override void Run()
+   {
+      string[] lines = ReadInputFile();
+
+      Console.WriteLine("Part 1:");
+      Console.WriteLine(GetGrandTotal(lines));
+
+      // Console.WriteLine("Part 2:");
+      // Console.WriteLine(GetCountOfIngredientsConsideredToBeFresh(lines));
+   }
+
+   public static long GetGrandTotal(string[] lines)
+   {
+      var problems = ExtractProblems(lines);
+      return problems.Sum(p => SolveProblem(p.numbers, p.operation));
+   }
+
+   private static List<(List<long> numbers, string operation)> ExtractProblems(string[] lines)
+   {
+      var converted = lines.Select(l => l.Split(" ", StringSplitOptions.RemoveEmptyEntries)).ToList();
+      var problems = new List<List<string>>();
+
+      for (int j = 0; j < converted[0].Length; j++)
+      {
+         List<string> problem = [];
+         for (int i = 0; i < lines.Length; i++)
+         {
+            problem.Add(converted[i][j]);
+         }
+         problems.Add(problem);
+      }
+
+      return problems.Select(p => (p[..^1].Select(long.Parse).ToList(), p[^1])).ToList();
+   }
+
+   private static long SolveProblem(List<long> numbers, string operation)
+   {
+      return operation switch
+      {
+         "+" => numbers.Sum(),
+         "*" => numbers.Aggregate((acc, cur) => acc * cur),
+         _ => throw new InvalidOperationException()
+      };
+   }
+
+}
