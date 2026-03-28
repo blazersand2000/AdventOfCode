f2b6cf661c3755811cdb793af67bcf5266e367f6
Andrew <blazersand2000@yahoo.com>
2026-03-28 13:37:25 -0400

    Finished day 8

---
+++ AdventOfCode.Tests/Aoc2025/Day8Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using AdventOfCode.Aoc2025.Day8;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2025;
+
+[TestFixture]
+class Day8Tests
+{
+   readonly string[] testData =
+   [
+      "162,817,812",
+      "57,618,57",
+      "906,360,560",
+      "592,479,940",
+      "352,342,300",
+      "466,668,158",
+      "542,29,236",
+      "431,825,988",
+      "739,650,466",
+      "52,470,668",
+      "216,146,977",
+      "819,987,18",
+      "117,168,530",
+      "805,96,715",
+      "346,949,466",
+      "970,615,88",
+      "941,993,340",
+      "862,61,35",
+      "984,92,344",
+      "425,690,689"
+   ];
+
+   [SetUp]
+   public void Setup()
+   {
+   }
+
+   [Test]
+   public void SizeOf3LargestCircuitsMultipliedTogether()
+   {
+      var result = Day8.SizeOf3LargestCircuitsMultipliedTogether(testData, 10);
+
+      Assert.AreEqual(40, result);
+   }
+
+   [Test]
+   public void LastTwoJunctionBoxesXCoordinatesMultiplied()
+   {
+      var result = Day8.LastTwoJunctionBoxesXCoordinatesMultiplied(testData);
+
+      Assert.AreEqual(25272, result);
+   }
+}
---
+++ AdventOfCode/Aoc2025/Day8/Day8.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace AdventOfCode.Aoc2025.Day8;
+
+public class Day8 : Problem
+{
+   public override void Run()
+   {
+      string[] lines = ReadInputFile();
+
+      Console.WriteLine("Part 1:");
+      Console.WriteLine(SizeOf3LargestCircuitsMultipliedTogether(lines, 1000));
+
+      Console.WriteLine("Part 2:");
+      Console.WriteLine(LastTwoJunctionBoxesXCoordinatesMultiplied(lines));
+   }
+
+   public static long SizeOf3LargestCircuitsMultipliedTogether(string[] lines, int numberOfConnections)
+   {
+      var points = ExtractPoints(lines);
+
+      var pairs = GetPairsOrderedByDistance(points.ToList());
+
+      var circuits = points.Select(p => new HashSet<Point> { p }).ToList();
+
+      for (int i = 0; i < numberOfConnections; i++)
+      {
+         var pair = pairs[i];
+         ConnectPair(circuits, pair);
+      }
+
+      var sizesOf3LargestCircuits = circuits.OrderByDescending(c => c.Count).Take(3).Select(c => c.Count).ToList();
+
+      return sizesOf3LargestCircuits[0] * sizesOf3LargestCircuits[1] * sizesOf3LargestCircuits[2];
+   }
+
+   public static long LastTwoJunctionBoxesXCoordinatesMultiplied(string[] lines)
+   {
+      var points = ExtractPoints(lines);
+
+      List<Pair> pairs = GetPairsOrderedByDistance(points.ToList());
+
+      var circuits = points.Select(p => new HashSet<Point> { p }).ToList();
+
+      foreach (var pair in pairs)
+      {
+         ConnectPair(circuits, pair);
+
+         if (circuits.Count == 1)
+         {
+            return pair.A.X * pair.B.X;
+         }
+      }
+
+      throw new InvalidOperationException("Somehow we connected all the junction boxes together and are not on one circuit. Impossible!");
+   }
+
+   private static HashSet<Point> ExtractPoints(string[] lines)
+   {
+      return lines.Select(l =>
+      {
+         var splits = l.Split(',').Select(s => long.Parse(s)).ToList();
+         return new Point(splits[0], splits[1], splits[2]);
+      }).ToHashSet();
+   }
+
+   private static List<Pair> GetPairsOrderedByDistance(List<Point> points)
+   {
+      var pairs = new List<Pair>();
+
+      for (int i = 0; i < points.Count; i++)
+      {
+         for (int j = i + 1; j < points.Count; j++)
+         {
+            var a = points[i];
+            var b = points[j];
+            var pair = new Pair(a, b);
+            pairs.Add(pair);
+         }
+      }
+
+      pairs = pairs.OrderBy(p => p.Distance()).ToList();
+      return pairs;
+   }
+
+   private static void ConnectPair(List<HashSet<Point>> circuits, Pair pair)
+   {
+      var circuitContainingA = circuits.Single(hs => hs.Contains(pair.A));
+      var circuitContainingB = circuits.Single(hs => hs.Contains(pair.B));
+
+      if (circuitContainingA == circuitContainingB)
+      {
+         return;
+      }
+
+      var mergedCircuit = circuitContainingA.Union(circuitContainingB).ToHashSet();
+      circuits.Remove(circuitContainingA);
+      circuits.Remove(circuitContainingB);
+      circuits.Add(mergedCircuit);
+   }
+
+   public readonly record struct Point(long X, long Y, long Z);
+
+   public readonly record struct Pair(Point A, Point B)
+   {
+      public Pair Reversed => new Pair(B, A);
+      public double Distance()
+      {
+         var dx = B.X - A.X;
+         var dy = B.Y - A.Y;
+         var dz = B.Z - A.Z;
+
+         return Math.Sqrt(dx * dx + dy * dy + dz * dz);
+      }
+   };
+}
