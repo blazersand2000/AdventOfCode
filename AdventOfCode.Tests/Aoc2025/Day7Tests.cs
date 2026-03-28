78c67e1ef49aa6201026da513b48133071efd460
Andrew <blazersand2000@yahoo.com>
2026-03-28 10:59:58 -0400

    Finished day 7

---
+++ AdventOfCode.Tests/Aoc2025/Day7Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using AdventOfCode.Aoc2025.Day7;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Aoc2025;
+
+[TestFixture]
+class Day7Tests
+{
+   readonly string[] testData =
+   [
+      ".......S.......",
+      "...............",
+      ".......^.......",
+      "...............",
+      "......^.^......",
+      "...............",
+      ".....^.^.^.....",
+      "...............",
+      "....^.^...^....",
+      "...............",
+      "...^.^...^.^...",
+      "...............",
+      "..^...^.....^..",
+      "...............",
+      ".^.^.^.^.^...^.",
+      "..............."
+   ];
+
+   [SetUp]
+   public void Setup()
+   {
+   }
+
+   [Test]
+   public void Part1()
+   {
+      var result = Day7.Part1(testData);
+
+      Assert.AreEqual(21, result);
+   }
+
+   [Test]
+   public void Part2()
+   {
+      var result = Day7.Part2(testData);
+
+      Assert.AreEqual(40, result);
+   }
+}
---
+++ AdventOfCode/Aoc2025/Day7/Day7.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace AdventOfCode.Aoc2025.Day7;
+
+public class Day7 : Problem
+{
+   public override void Run()
+   {
+      string[] lines = ReadInputFile();
+
+      Console.WriteLine("Part 1:");
+      Console.WriteLine(Part1(lines));
+
+      Console.WriteLine("Part 2:");
+      Console.WriteLine(Part2(lines));
+   }
+
+   public static int Part1(string[] lines)
+   {
+      var grid = lines.Select(l => l.ToCharArray()).ToArray();
+      return GetNumberOfSplits(grid, 1);
+   }
+
+   public static long Part2(string[] lines)
+   {
+      var grid = lines.Select(l => l.ToCharArray()).ToArray();
+      var start = new Coordinate(1, lines[0].IndexOf('S'));
+      return GetNumberOfTimelines(grid, start, new Dictionary<Coordinate, long>());
+   }
+
+   public static int GetNumberOfSplits(char[][] grid, int currentRow)
+   {
+      if (currentRow >= grid.Length)
+      {
+         return 0;
+      }
+
+      List<int> splitIndices = [];
+
+      for (int j = 0; j < grid[currentRow].Length; j++)
+      {
+         if (grid[currentRow - 1][j] is 'S' or '|')
+         {
+            if (grid[currentRow][j] == '^')
+            {
+               splitIndices.Add(j);
+               grid[currentRow][j - 1] = '|';
+               grid[currentRow][j + 1] = '|';
+            }
+            else
+            {
+               grid[currentRow][j] = '|';
+            }
+         }
+      }
+
+      return splitIndices.Count + GetNumberOfSplits(grid, currentRow + 1);
+   }
+
+   public static long GetNumberOfTimelines(char[][] grid, Coordinate current, Dictionary<Coordinate, long> memo)
+   {
+      if (memo.TryGetValue(current, out var numberOfTimelines))
+      {
+         return numberOfTimelines;
+      }
+
+      if (current.Row >= grid.Length)
+      {
+         memo[current] = 1;
+         return 1;
+      }
+
+      if (grid[current.Row][current.Col] == '^')
+      {
+         var left = GetNumberOfTimelines(grid, current with { Row = current.Row + 1, Col = current.Col - 1 }, memo);
+         var right = GetNumberOfTimelines(grid, current with { Row = current.Row + 1, Col = current.Col + 1 }, memo);
+         memo[current] = left + right;
+
+         return left + right;
+      }
+
+      numberOfTimelines = GetNumberOfTimelines(grid, current with { Row = current.Row + 1 }, memo);
+      memo[current] = numberOfTimelines;
+
+      return numberOfTimelines;
+   }
+
+   public readonly record struct Coordinate(int Row, int Col);
+}
